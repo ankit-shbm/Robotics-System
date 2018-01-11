cb944d633aa112da53ff86bcd3d271a64b760412
ANKIT KUMAR <ankit247shbm@gmail.com>
2018-01-11 19:39:10 +0530

    Update BitRelation.java

--- BitRelation.java
+++ BitRelation.java
@@ -1,7 +1,7 @@
 /***** An Algorithm that predicts the Relationship among the BitStrings from a given Set.*****/
 /* Basic Operations: 'AND', 'NOT', 'XOR', 'OR'. */
-/* Total numbers of Operations between 2-bit may be Atmost 8[pow(2, 3)]. */
-/* For the String of n-bit length is Atmost [8 * (n - 1)]. */
+/* Total numbers of Operations between 2-bit may be atmost 8[pow(2, 3)]. */
+/* For the String of n-bit length is Max. Operations atmost [8 * (n - 1)]. */
 /* For 128-Bit, Max. Operations = 1016(approx.[pow(2, 9.9887)]). */
 //------------------------------------------------------------------------------------------------------
 
@@ -12,5 +12,6 @@ public class BitRelation
        String string = new String();
        string = "011101010101111000111101010101010111000001101111101010000010101010101010111000010010101001101010";
        System.out.println(string.length());
+    
     }   
  }
