79764cebdd83c6a2adc2cb81b72fa3ac991e9b30
ANKIT KUMAR <ankit247shbm@gmail.com>
2018-01-11 14:47:38 +0530

    Update BitRelation.java

--- BitRelation.java
+++ BitRelation.java
@@ -2,5 +2,5 @@
 /* Basic Operations: 'AND', 'NOT', 'XOR', 'OR'. */
 /* Total numbers of Operations between 2-bit may be Atmost 8[pow(2, 3)]. */
 /* For the String of n-bit length is Atmost [8 * (n - 1)]. */
-/* For 128-Bit, Max. Operation = 1016. */
+/* For 128-Bit, Max. Operation = 1016(approx.[pow(2, 9.9887)]). */
 
