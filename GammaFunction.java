ab859364350aeb3377dfa39b8d9437b39463008b
ANKIT KUMAR <ankit247shbm@gmail.com>
2018-01-21 00:15:12 +0530

    Update GammaFunction.java

--- GammaFunction.java
+++ GammaFunction.java
@@ -1,4 +1,21 @@
 /***** Development of Gamma-Function.*****/
 /* Using Java OOPs, Java STL and Advanced Java Data STructure. */
 
-public class  
+import java.io.*;  
+import java.net.*;  
+
+public class GammaFunction
+{
+    public static void main(String[] args)
+    {
+        double i = 0;
+        double count = 0;
+        while(i < 600)
+        {
+            i = i + .001;
+            System.out.println(i);
+            count++;
+        }
+//        System.out.println(" " + count);
+    }
+}
